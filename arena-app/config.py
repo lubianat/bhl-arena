bcf7a8e868c9f67f97e86392d4a5232576551858
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2025-01-09 13:19:44 -0300

    chore: Reorganize repository

--- arena-app/config.py
+++
@@ -1,9 +0,0 @@
-"""
-This file contains application configuration settings.
-"""
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///arena.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev')
